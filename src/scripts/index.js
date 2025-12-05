4e0b82015d277d2f05df3f4b3736fac70bb876c9
Prasadmukalla12 <prasadmukalla124@gmail.com>
2025-12-05 11:44:35 +0530

    cc

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,12 +16,19 @@ function Project5Click(){
 
 function CategoryClick(value){
     var car1 = document.getElementById("fullstack1")
-    var car2 = document.getElementById("fullstack1")
+    var car2 = document.getElementById("fullstack2")
     var car3 = document.getElementById("frontend1")
     var car4 = document.getElementById("frontend2")
     var car5 = document.getElementById("frontend3")
+    var btn1 = document.getElementById("all")
+    var btn2 = document.getElementById("fulstk")
+    var btn3 = document.getElementById("frnEnd")
     switch(value.id){
         case "frnEnd":
+            btn3.style.color = "white"
+            btn3.style.backgroundColor = "orange"
+            btn2.style.backgroundColor = "black"
+            btn1.style.backgroundColor = "black"
             car1.style.display = "none"
             car2.style.display = "none"
             car3.style.display = "block"
@@ -29,6 +36,10 @@ function CategoryClick(value){
             car5.style.display = "block"
             break;
         case "fulstk" : 
+            btn2.style.color = "white"
+            btn2.style.backgroundColor = "orange"
+            btn3.style.backgroundColor = "black"
+            btn1.style.backgroundColor = "black"
             car1.style.display = "block"
             car2.style.display = "block"
             car3.style.display = "none"
@@ -36,6 +47,10 @@ function CategoryClick(value){
             car5.style.display = "none"
             break;
         case "all":
+            btn1.style.color = "white"
+            btn1.style.backgroundColor = "orange"
+            btn2.style.backgroundColor = "black"
+            btn3.style.backgroundColor = "black"
             car1.style.display = "block"
             car2.style.display = "block"
             car3.style.display = "block"
